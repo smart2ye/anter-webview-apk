6cb3dfe01a1912b603103e2f1c2893120deb7d12
smart2ye <smart2ye@gmail.com>
2026-09-25 22:55:00 +0300

    Move server button to bottom-right with visible circular background + bump 1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.smart2ye.anter"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.6"
+        versionCode = 5
+        versionName = "1.0.7"
     }
 
     signingConfigs {
