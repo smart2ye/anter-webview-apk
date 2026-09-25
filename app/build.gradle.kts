677254ce253afeb42ba9755cef5fbe583a0c18ee
smart2ye <smart2ye@gmail.com>
2026-09-25 22:50:47 +0300

    Allow cleartext to localhost for local server testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.smart2ye.anter"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.5"
+        versionCode = 4
+        versionName = "1.0.6"
     }
 
     signingConfigs {
